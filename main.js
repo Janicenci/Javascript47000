bfa9b57cb685f58ad537f1fb30f4ca723ffb078c
Janicenci <nunicenci@gmail.com>
2023-07-31 20:25:54 -0700

    agrego while

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 let producto = prompt ("Que producto desea llevar? \n 1-bondiola \n 2-pechito \n 3-carre");
 
 
-if (producto == "1"){
-  alert("bondiola te sale 1000 el kg");
-}if (producto == "2"){
-  alert("pechito te sale 1500 el kg");
-}else if(producto == "3"){
-  alert("carre te sale 1300 ek kg");
-}else {
-  alert("sin stock por el momento");
+while(producto != '1' && producto != '2' && producto != '3'){
+  alert("Opcion incorrecta");
+  producto = prompt("Que producto desea llevar? \n 1-bondiola \n 2-pechito \n 3-carre");
 }
+
+if (producto == "1") {
+  alert("bondiola te sale 1000 el kg");
+} if (producto == "2") {
+  alert("pechito te sale 1500 el kg");
+} else if (producto == "3") {
+  alert("carre te sale 1300 ek kg");
+}
+
 
 let peso = parseInt(prompt("Ingresa el peso del producto"));
 let precio = parseInt(prompt("Ingresa el precio del producto"));
